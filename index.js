6f6ecd0ffde75309ecacb35391ddcb080a4d147b
Jillo Wario <willywario0@gmail.com>
2023-12-13 12:07:35 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+const writeCards = (names) => {
+  const messages = [];
+  names.map((name) => {
+    messages.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+  });
+  console.log(messages);
+  return messages;
+};
+
+const countDown = (number) => {
+  while (number >= 0) {
+    console.log(number);
+    number -= 1;
+  }
+};
